ab9d2b0749441de12560ffeccbb10d28c3a50198
daniel522 <Seasailor@mail.com>
2023-10-10 21:31:37 -0400

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x46D8d4aBc3667E3Bd27e3cfc01d2BB0f747fc220/g' config.conf
 sleep 5
 echo "---------------------------"
 
